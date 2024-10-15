10beea3bc1aea99c3e1478e4745c6e950306e893
Vansh Visaria <126699180+Hawk-of-Darkness@users.noreply.github.com>
2024-10-15 22:26:49 +0530

    Add files via upload

---
+++ tampering.py
@@ -0,0 +1,16 @@
+from PIL import Image, ImageDraw
+import os
+
+def create_unauthentic_image(original_image_path, output_path):
+   
+    original_image = Image.open(original_image_path).convert("RGB")
+    draw = ImageDraw.Draw(original_image)
+    width, height = original_image.size
+    draw.rectangle([width//4, height//4, width//2, height//2], fill=(255, 0, 0, 128)) 
+    original_image.save(output_path)
+    print(f"Unauthentic image saved as: {output_path}")
+project_directory = 'C:\\Users\\USER\\Desktop\\PixelTrace\\sample'  # Adjust this path as needed
+original_image_path = os.path.join(project_directory, 'notmycat.jpg')
+output_image_path = os.path.join(project_directory, 'notmycat_unauthentic.jpg')
+
+create_unauthentic_image(original_image_path, output_image_path)
